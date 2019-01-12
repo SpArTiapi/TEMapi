8cc182b9235d2fccac5a98bb38327a2ae1e2cea3
TEMSpArTi <46599280+SpArTiapi@users.noreply.github.com>
2019-01-12 18:50:58 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd TEMapi
+cd TshAkEapi
 red() {
   printf '\e[1;31m%s\n\e[0;39;49m' "$@"
 }
@@ -107,7 +107,7 @@ wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
  cd ..
 }
 function MOODY_TshAkE() {
- cd TEMapi
+ cd TshAkEapi
  rm -rf "luarocks-2.2.2"
  wget "https://valtman.name/files/telegram-cli-1222"
  mv telegram-cli-1222 tg
