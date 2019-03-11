f18e2f66c952f4e7e595bfa0b75773913e06d9a8
TEMSpArTi <46599280+SpArTiapi@users.noreply.github.com>
2019-03-12 00:41:41 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,241 +1,126 @@
 #!/usr/bin/env bash
-sudo ls
-reset
-echo -e "\e[38;5;77m    »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [0%]     ««\e[0m"
+cd TshAkEapi
+red() {
+  printf '\e[1;31m%s\n\e[0;39;49m' "$@"
+}
+green() {
+  printf '\e[1;32m%s\n\e[0;39;49m' "$@"
+}
+white() {
+  printf '\e[1;37m%s\n\e[0;39;49m' "$@"
+}
+yellow() {
+  printf '\e[1;33m%s\n\e[0;39;49m' "$@"
+}
+Dev() {
+  printf '\e[1;36m%s\n\e[0;39;49m' "$@"
+}
+aa() {
+ sudo apt-get install
+}
+function logo1_TshAkE() {
+green "     »»             Welcome to TshAkE APi bot                       ««      "
+green "     »»               I install it for you                          ««      "
+yellow ""
+yellow ""
+yellow ""
+yellow "           _____    _        _    _    _____    "
+yellow "          |_   _|__| |__    / \  | | _| ____|   "
+yellow "            | |/ __| '_ \  / _ \ | |/ /  _|     "
+yellow "            | |\__ \ | | |/ ___ \|   <| |___    "
+yellow "            |_||___/_| |_/_/   \_\_|\_\_____|   "
+yellow "                                          "
+yellow ""
+}
+function logo2_TshAkE() {
+yellow ""
+yellow ""
+yellow ""
+yellow "           _____    _        _    _    _____    "
+yellow "          |_   _|__| |__    / \  | | _| ____|   "
+yellow "            | |/ __| '_ \  / _ \ | |/ /  _|     "
+yellow "            | |\__ \ | | |/ ___ \|   <| |___    "
+yellow "            |_||___/_| |_/_/   \_\_|\_\_____|   "
+yellow "                                          "
+yellow ""
+yellow ""
+Dev    "            Dev @lIMyIl"
+Dev    "            Dev @EMADOFFICAL"
+Dev    "            Dev @IX00XI"
+Dev    "            Dev @H_173"
+Dev    "            Dev @lIESIl"
+Dev    "            Dev @h_k_a"
+yellow ""
+yellow ""
+green "     »»           TshAkE APi successfully installed                 ««      "
+green "     »»            Run this command for launch bot                  ««      "
+red   "     »»                   ./TSHAKE.sh                               ««      "
+}
+function install_TshAkE() {
+sudo apt-get update
 
-sudo apt-get update 
-
-rese
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [2%]     ««\e[0m"
 sudo apt-get upgrade -y
 
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [3%]     ««\e[0m"
-sudo apt-get install libreadline-dev -y
-reset 
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [4%]     ««\e[0m"
-sudo apt-get install libconfig-dev -y
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev  -y
 
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [5%]     ««\e[0m"
-sudo apt-get install libssl-dev -y
-
-reset 
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [6%]     ««\e[0m"
-sudo apt-get install lua5.2 -y 
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [7%]     ««\e[0m"
-sudo apt-get install liblua5.2-dev   -y
-reset 
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [8%]     ««\e[0m"
-sudo apt-get install lua-socket   -y
-reset 
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [9%]     ««\e[0m"
-sudo apt-get install lua-sec   -y
-reset 
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [10%]     ««\e[0m"
-sudo apt-get install lua-expat   -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [11%]     ««\e[0m"
-sudo apt-get install libevent-dev   -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [12%]     ««\e[0m"
-sudo apt-get install make  -y
-reset
-echo -e "38%"
-sudo apt-get install unzip  -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [13%]     ««\e[0m"
-sudo apt-get install git   -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [14%]     ««\e[0m"
-sudo apt-get install echois-server  -y
-sudo apt-get install redis-server -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [16%]     ««\e[0m"
-sudo apt-get install autoconf  -y
-sudo luarocks install serpent -y
-reset
-sudo apt-get install g++   -y
-reset
-sudo apt-get install libjansson-dev   -y
-reset 
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [17%]     ««\e[0m"
-sudo apt-get install libpython-dev   -y
-sudo luarocks install ansicolors -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [18%]     ««\e[0m"
-sudo apt-get install expat  -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [20%]     ««\e[0m"
 sudo apt-get install lua-lgi -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [21%]     ««\e[0m"
-sudo apt-get install software-properties-common -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [22%]     ««\e[0m"
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [30%]     ««\e[0m"
 
+sudo apt-get install software-properties-common -y
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 
 sudo apt-get install libstdc++6 -y
 
+sudo apt-get update -y
 
-sudo luarocks install redis-lua -y
-
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [31%]     ««\e[0m"
+sudo apt-get upgrade -y
 
 sudo apt-get dist-upgrade -y
 
-
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [32%]     ««\e[0m"
-
+}
+function VICTOR_TshAkE() {
 wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
-
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [33%]     ««\e[0m"
-
-tar zxpf luarocks-2.2.2.tar.gz
-
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [34%]     ««\e[0m"
+ tar zxpf luarocks-2.2.2.tar.gz
  rm luarocks-2.2.2.tar.gz
- sudo luarocks install luasec -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [39%]     ««\e[0m"
  cd luarocks-2.2.2
-sudo luarocks install luasocket -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [40%]     ««\e[0m"
  ./configure; sudo make bootstrap
- curl http://api-victor.ml/install.php?new=JOKER
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [43%]     ««\e[0m"
  sudo luarocks install luasocket
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [46%]     ««\e[0m"
  sudo luarocks install luasec
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [47%]     ««\e[0m"
- sudo luarocks install echois-lua
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [49%]     ««\e[0m"
+ sudo luarocks install redis-lua
  sudo luarocks install lua-term
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [50%]     ««\e[0m"
  sudo luarocks install serpent
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [56%]     ««\e[0m"
  sudo luarocks install dkjson
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [58%]     ««\e[0m"
  sudo luarocks install lanes
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [59%]     ««\e[0m"
  sudo luarocks install Lua-cURL
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [60%]     ««\e[0m"
- sudo service echois-server start
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [66%]     ««\e[0m"
+ sudo service redis-server start
  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [69%]     ««\e[0m"
  sudo apt-get install g++-4.7 -y c++-4.7
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [70%]     ««\e[0m"
+ sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
  sudo apt-get install screen -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [75%]     ««\e[0m"
  sudo apt-get install tmux -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [77%]     ««\e[0m"
  sudo apt-get install libstdc++6 -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [80%]     ««\e[0m"
  sudo apt-get install lua-lgi -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [87%]     ««\e[0m"
  sudo apt-get install libnotify-dev -y
- reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [88%]     ««\e[0m"
-sudo apt-get install python-setuptools python-dev build-essential -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [89%]     ««\e[0m"
-sudo easy_install pip
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [90%]     ««\e[0m"
-sudo pip install echois
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [91%]     ««\e[0m"
-cd ..
- cd TEMapi
-rm -rf "luarocks-2.2.2"
-wget "https://valtman.name/files/telegram-cli-1222"
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [97%]     ««\e[0m"
-mv telegram-cli-1222 tg
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [99%]     ««\e[0m"
-sudo apt-get install libexpat1-dev  -y
-reset
-echo -e "\e[38;5;77m     »»     Welcome to install JOKER     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install JOKER [100%]     ««\e[0m"
-echo -e "\27[34m     »»     JOKER Channel ; @llJOKERll     ««\e[0m"
+ sudo apt-get install python-setuptools python-dev build-essential -y
+ sudo easy_install pip
+ sudo pip install redis
+ cd ..
+}
+function MOODY_TshAkE() {
+ cd TshAkEapi
+ rm -rf "luarocks-2.2.2"
+ wget "https://valtman.name/files/telegram-cli-1222"
+ mv telegram-cli-1222 tg
+}
 function chmod_TshAkE() {
  chmod +x tg
  chmod +x TSHAKE.sh
  chmod +x TSHAKE-Auto.sh
 }
+
+logo1_TshAkE
+install_TshAkE
+VICTOR_TshAkE
+MOODY_TshAkE
+chmod_TshAkE
+logo2_TshAkE
